2db915a9c93f8a416cf6df0d865f0e02e06aee99
Amadewe <dewevre.amandine@gmail.com>
2019-02-13 14:38:04 +0100

    Modif Ama

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#priceone').click(function(){
+var priceone = $(this).val();
+// il faut enlever l'alert quand on aura crée la fenêtre modale....
+alert('le prix est de :' + priceone);
+  });
+});
