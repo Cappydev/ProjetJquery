ef1aab3daf7b6c9b8f5e86e0af3d98c55ac1313d
cappydev <kevin.lanel@wanadoo.fr>
2019-02-13 15:12:13 +0100

    projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-$(function(){
-  $('.bRacket').click(function(){
+$(function() {
+  $('.bRacket').click(function() {
     $('.bRacketProduct').show();
     $('.bWearProduct').hide();
   });
-  $('.bWear').click(function(){
+  $('.bWear').click(function() {
     $('.bRacketProduct').hide();
     $('.bWearProduct').show();
+  });
+  $(".navbar a, footer a").on("click", function(event) {
+
+    event.preventDefault();
+    var hash = this.hash;
+
+    $('body,html').animate({
+      scrollTop: $(hash).offset().top
+    }, 1500, function() {
+      window.location.hash = hash;
+    });
+
   });
 });
