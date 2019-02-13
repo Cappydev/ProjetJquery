4329e339f6d36ba54e29572392d4623a7193025d
Amadewe <dewevre.amandine@gmail.com>
2019-02-13 15:48:43 +0100

    Changement des id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('#priceone').click(function(){
+
 var priceone = $(this).val();
 // il faut enlever l'alert quand on aura crée la fenêtre modale....
 alert('le prix est de :' + priceone);
